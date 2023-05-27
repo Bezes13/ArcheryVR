6b3519a1b5b25a197ecbc92853eef15b1ce823f9
alexander.belzer <alexander.belzer@innogames.com>
2023-05-27 22:28:07 +0200

    reverted some things

--- Archery/Assets/Scripts/Delaunay.cs
+++ Archery/Assets/Scripts/Delaunay.cs
@@ -18,7 +18,7 @@ public class Delaunay
             new Vector3(scl * 3, 0, 0),
             new Vector3(0, scl * 3, 0),
             new Vector3(0, 0, scl * 3));
-        Nodes = new List<DelaunayNode> {new(root.a, root.b, root.c, root.d)};
+        Nodes = new List<DelaunayNode> {new (root.a,root.b, root.c, root.d)};
         Debug.Log(target.transform.localScale.x);
         // Generate Random Points and add them to Delaunay 
         for (var i = 0; i < num; i++)
@@ -27,38 +27,55 @@ public class Delaunay
                 UnityEngine.Random.value * scl);
             //p = target.GetComponent<MeshFilter>().mesh.bounds.ClosestPoint(p);
             // Split(p*target.transform.localScale.x); 
-
+        
             AddPoint(p);
         }
     }
 
-    private void AddPoint(Vector3 p)
+    void AddPoint(Vector3 p)
     {
         var n = Nodes.Find(tetra => tetra.Tetrahedra.Contains(p));
         var splitTuple = n.Split(p);
         Nodes.Remove(n);
-        Nodes.AddRange(splitTuple.Item2);
-        foreach (var triangle in splitTuple.Item1)
-        {
-            _stack.Push(triangle);
-        }
-
+        Nodes.Add(splitTuple.n1);
+        Nodes.Add(splitTuple.n2);
+        Nodes.Add(splitTuple.n3);
+        Nodes.Add(splitTuple.n4);
+        _stack.Push(splitTuple.t1);
+        _stack.Push(splitTuple.t2);
+        _stack.Push(splitTuple.t3);
+        _stack.Push(splitTuple.t4);
         // Go through the new added tetras
         while (_stack.Count > 0)
         {
             var t = _stack.Pop();
             // Find tetras which contain the triangle face
-            var nodes = FindNodes(t);
-            if (nodes.Count == 0) continue;
+            if (!FindNodes(t, out DelaunayNode n1, out DelaunayNode n2)) continue;
             // get the remaining points
-            var p1 = nodes[0].Tetrahedra.RemainingPoint(t);
-            var p2 = nodes[1].Tetrahedra.RemainingPoint(t);
-            
-            // If is Delaunay then we can skip this
-            if (!nodes[0].Tetrahedra.GetSphere().Contains(p2)) continue;
-            
-            if (!t.Intersects(new Line(p1, p2), out var i))
+            var p1 = n1.Tetrahedra.RemainingPoint(t);
+            var p2 = n2.Tetrahedra.RemainingPoint(t);
+            // if the tetra sphere contains the dot
+            //      if the line between the remaining points intersects the tetra
+            //            do flip, remove triangles from stack and add tetras to nodes
+            //      else find Intersecting Point
+            //          do flip on other things
+            if (!n1.Tetrahedra.GetSphere().Contains(p2)) continue;
+            if (t.Intersects(new Line(p1, p2), out Vector3 i))
             {
+                var o = DelaunayNode.Flip23(n1, n2, p1, p2, t);
+                _stack.Push(o.t1);
+                _stack.Push(o.t2);
+                _stack.Push(o.t3);
+                _stack.Push(o.t4);
+                _stack.Push(o.t5);
+                _stack.Push(o.t6);
+                Nodes.Remove(n1);
+                Nodes.Remove(n2);
+                Nodes.Add(o.n1);
+                Nodes.Add(o.n2);
+                Nodes.Add(o.n3);
+            }
+            else {
                 Vector3 far;
                 if (Line.IsIntersecting(new Line(i, t.a), new Line(t.b, t.c))) far = t.a;
                 else if (Line.IsIntersecting(new Line(i, t.b), new Line(t.c, t.a))) far = t.b;
@@ -67,86 +84,40 @@ public class Delaunay
 
                 var cm = t.Remaining(far);
                 var t3 = new Triangle(cm, p1);
-                var n3 = nodes[0].GetFacingNode(t3);
+                var n3 = n1.GetFacingNode(t3);
 
                 if (!Equals(n3.Tetrahedra.RemainingPoint(t3), p2)) continue;
-                
-                var o = Flip32(nodes[0], nodes[1], n3, p1, far, p2, cm.a, cm.b);
-                FlipResult(o);
-            }
-            else
-            {
-                var o = Flip23(nodes[0], nodes[1], p1, p2, t);
-                FlipResult(o);
-            }
-            
-            void FlipResult((List<Triangle>, List<DelaunayNode>) flip)
-            {
-                foreach (var triangle in flip.Item1)
-                {
-                    _stack.Push(triangle);
-                }
 
-                Nodes.Remove(nodes[0]);
-                Nodes.Remove(nodes[1]);
-                Nodes.AddRange(flip.Item2);
+                var p12 = far;
+                var p23 = p2;
+                var p31 = p1;
+                var o = DelaunayNode.Flip32(n1, n2, n3, p31, p12, p23, cm.a, cm.b);
+                _stack.Push(o.t1);
+                _stack.Push(o.t2);
+                _stack.Push(o.t3);
+                _stack.Push(o.t4);
+                _stack.Push(o.t5);
+                _stack.Push(o.t6);
+                Nodes.Remove(n1);
+                Nodes.Remove(n2);
+                Nodes.Remove(n3);
+                Nodes.Add(o.n1);
+                Nodes.Add(o.n2);
             }
         }
     }
 
-    private List<DelaunayNode> FindNodes(Triangle t)
+    bool FindNodes(Triangle t, out DelaunayNode n1, out DelaunayNode n2)
     {
         var o = Nodes.FindAll(n => n.HasFacet(t));
-        return o.Count == 2 ? o : new List<DelaunayNode>();
-    }
-    
-    private static (List<Triangle>, List<DelaunayNode>) Flip23(DelaunayNode n1, DelaunayNode n2, Vector3 p1, Vector3 p2, Triangle t)
-    {
-        var nab = new DelaunayNode(p1, p2, t.a, t.b);
-        var nbc = new DelaunayNode(p1, p2, t.b, t.c);
-        var nca = new DelaunayNode(p1, p2, t.c, t.a);
-    
-        nab.neighbor = new List<DelaunayNode> { nbc, nca };
-        nbc.neighbor = new List<DelaunayNode> { nca, nab };
-        nca.neighbor = new List<DelaunayNode> { nab, nbc };
-    
-        var triangles = new List<Triangle>
+        if (o.Count == 2)
         {
-            new(t.a, t.b, p1), new(t.a, t.b, p2), new(t.b, t.c, p1),
-            new(t.b, t.c, p2), new(t.c, t.a, p1), new(t.c, t.a, p2)
-        };
-    
-        n1.SetNeighbor(nab, triangles[0]);
-        n2.SetNeighbor(nab, triangles[1]);
-        n1.SetNeighbor(nbc, triangles[2]);
-        n2.SetNeighbor(nbc, triangles[3]);
-        n1.SetNeighbor(nca, triangles[4]);
-        n2.SetNeighbor(nca, triangles[5]);
-    
-        return (triangles, new List<DelaunayNode> { nab, nbc, nca });
-    }
+            n1 = o[0];
+            n2 = o[1];
+            return true;
+        }
 
-    private static (List<Triangle>, List<DelaunayNode>) Flip32(DelaunayNode n1, DelaunayNode n2, DelaunayNode n3, Vector3 a, Vector3 b, Vector3 c, Vector3 x, Vector3 y)
-    {
-        var nx = new DelaunayNode(a, b, c, x);
-        var ny = new DelaunayNode(a, b, c, y);
-        nx.neighbor.Add(ny);
-        ny.neighbor.Add(nx);
-    
-        var triangles = new List<Triangle>
-        {
-            new(x, a, b), new(x, b, c), new(x, c, a),
-            new(y, a, b), new(y, b, c), new(y, c, a)
-        };
-    
-        n1.SetNeighbor(nx, triangles[0]);
-        n2.SetNeighbor(nx, triangles[1]);
-        n3.SetNeighbor(nx, triangles[2]);
-    
-        n1.SetNeighbor(ny, triangles[3]);
-        n2.SetNeighbor(ny, triangles[4]);
-        n3.SetNeighbor(ny, triangles[5]);
-    
-        return (triangles, new List<DelaunayNode> { nx, ny });
+        n1 = n2 = default;
+        return false;
     }
 }
--- Archery/Assets/Scripts/DelaunayNode.cs
+++ Archery/Assets/Scripts/DelaunayNode.cs
@@ -14,29 +14,81 @@ public class DelaunayNode
         neighbor = new List<DelaunayNode>(4);
     }
 
-    public (List<Triangle>, List<DelaunayNode>) Split(Vector3 p)
+    public (Triangle t1, Triangle t2, Triangle t3, Triangle t4, DelaunayNode n1, DelaunayNode n2, DelaunayNode n3,
+        DelaunayNode n4) Split(Vector3 p)
     {
-        var abc = new DelaunayNode(p, Tetrahedra.a, Tetrahedra.b, Tetrahedra.c);
-        var abcTriangle = new Triangle(Tetrahedra.a, Tetrahedra.b, Tetrahedra.c);
-        var bcd = new DelaunayNode(p, Tetrahedra.b, Tetrahedra.c, Tetrahedra.d);
-        var bcdTriangle =  new Triangle(Tetrahedra.b, Tetrahedra.c, Tetrahedra.d);
-
-        var cda = new DelaunayNode(p, Tetrahedra.c, Tetrahedra.d, Tetrahedra.a);
-        var cdaTriangle =  new Triangle(Tetrahedra.c, Tetrahedra.d, Tetrahedra.a);
-        var dab = new DelaunayNode(p, Tetrahedra.d, Tetrahedra.a, Tetrahedra.b);
-        var dabTriangle = new Triangle(Tetrahedra.d, Tetrahedra.a, Tetrahedra.b);
-        abc.neighbor = new List<DelaunayNode> {bcd, cda, dab};
-        bcd.neighbor = new List<DelaunayNode> {cda, dab, abc};
-        cda.neighbor = new List<DelaunayNode> {dab, abc, bcd};
-        dab.neighbor = new List<DelaunayNode> {abc, bcd, cda};
-        SetNeighbor(abc, abcTriangle);
-        SetNeighbor(bcd, bcdTriangle);
-        SetNeighbor(cda, cdaTriangle);
-        SetNeighbor(dab, dabTriangle);
-        return (new List<Triangle>(){abcTriangle, bcdTriangle, cdaTriangle, dabTriangle}, new List<DelaunayNode>(){ abc, bcd, cda, dab});
+        (DelaunayNode n, Triangle t) abc = (new DelaunayNode(p, Tetrahedra.a, Tetrahedra.b, Tetrahedra.c),
+            new Triangle(Tetrahedra.a, Tetrahedra.b, Tetrahedra.c));
+        (DelaunayNode n, Triangle t) bcd = (new DelaunayNode(p, Tetrahedra.b, Tetrahedra.c, Tetrahedra.d),
+            new Triangle(Tetrahedra.b, Tetrahedra.c, Tetrahedra.d));
+        (DelaunayNode n, Triangle t) cda = (new DelaunayNode(p, Tetrahedra.c, Tetrahedra.d, Tetrahedra.a),
+            new Triangle(Tetrahedra.c, Tetrahedra.d, Tetrahedra.a));
+        (DelaunayNode n, Triangle t) dab = (new DelaunayNode(p, Tetrahedra.d, Tetrahedra.a, Tetrahedra.b),
+            new Triangle(Tetrahedra.d, Tetrahedra.a, Tetrahedra.b));
+        abc.n.neighbor = new List<DelaunayNode> {bcd.n, cda.n, dab.n};
+        bcd.n.neighbor = new List<DelaunayNode> {cda.n, dab.n, abc.n};
+        cda.n.neighbor = new List<DelaunayNode> {dab.n, abc.n, bcd.n};
+        dab.n.neighbor = new List<DelaunayNode> {abc.n, bcd.n, cda.n};
+        SetNeighbor(abc.n, abc.t);
+        SetNeighbor(bcd.n, bcd.t);
+        SetNeighbor(cda.n, cda.t);
+        SetNeighbor(dab.n, dab.t);
+        return (abc.t, bcd.t, cda.t, dab.t, abc.n, bcd.n, cda.n, dab.n);
     }
-    
-    public void SetNeighbor(DelaunayNode n, Triangle t)
+
+    public static (Triangle t1, Triangle t2, Triangle t3, Triangle t4, Triangle t5, Triangle t6, DelaunayNode n1,
+        DelaunayNode n2, DelaunayNode n3)
+        Flip23(DelaunayNode n1, DelaunayNode n2, Vector3 p1, Vector3 p2, Triangle t)
+    {
+        var nab = new DelaunayNode(p1, p2, t.a, t.b);
+        var nbc = new DelaunayNode(p1, p2, t.b, t.c);
+        var nca = new DelaunayNode(p1, p2, t.c, t.a);
+        nab.neighbor = new List<DelaunayNode> {nbc, nca};
+        nbc.neighbor = new List<DelaunayNode> {nca, nab};
+        nca.neighbor = new List<DelaunayNode> {nab, nbc};
+        var t_ab_p1 = new Triangle(t.a, t.b, p1);
+        n1.SetNeighbor(nab, t_ab_p1);
+        var t_ab_p2 = new Triangle(t.a, t.b, p2);
+        n2.SetNeighbor(nab, t_ab_p2);
+        var t_bc_p1 = new Triangle(t.b, t.c, p1);
+        n1.SetNeighbor(nbc, t_bc_p1);
+        var t_bc_p2 = new Triangle(t.b, t.c, p2);
+        n2.SetNeighbor(nbc, t_bc_p2);
+        var t_ca_p1 = new Triangle(t.c, t.a, p1);
+        n1.SetNeighbor(nca, t_ca_p1);
+        var t_ca_p2 = new Triangle(t.c, t.a, p2);
+        n2.SetNeighbor(nca, t_ca_p2);
+        return (t_ab_p1, t_ab_p2, t_bc_p1, t_bc_p2, t_ca_p1, t_ca_p2, nab, nbc, nca);
+    }
+
+    public static (Triangle t1, Triangle t2, Triangle t3, Triangle t4, Triangle t5, Triangle t6, DelaunayNode n1,
+        DelaunayNode n2)
+        Flip32(DelaunayNode n1, DelaunayNode n2, DelaunayNode n3, Vector3 p31, Vector3 p12, Vector3 p23, Vector3 apex_x,
+            Vector3 apex_y)
+    {
+        var a = p31;
+        var b = p12;
+        var c = p23;
+        var nx = new DelaunayNode(a, b, c, apex_x);
+        var ny = new DelaunayNode(a, b, c, apex_y);
+        nx.neighbor.Add(ny);
+        ny.neighbor.Add(nx);
+        var xab = new Triangle(apex_x, a, b);
+        var yab = new Triangle(apex_y, a, b);
+        var xbc = new Triangle(apex_x, b, c);
+        var ybc = new Triangle(apex_y, b, c);
+        var xca = new Triangle(apex_x, c, a);
+        var yca = new Triangle(apex_y, c, a);
+        n1.SetNeighbor(nx, xab);
+        n2.SetNeighbor(nx, xbc);
+        n3.SetNeighbor(nx, xca);
+        n1.SetNeighbor(ny, yab);
+        n2.SetNeighbor(ny, ybc);
+        n3.SetNeighbor(ny, yca);
+        return (xab, xbc, xca, yab, ybc, yca, nx, ny);
+    }
+
+    private void SetNeighbor(DelaunayNode n, Triangle t)
     {
         var pair = GetFacingNode(t);
         if (pair == null)
