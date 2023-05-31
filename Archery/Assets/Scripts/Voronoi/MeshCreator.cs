233600f725bab7b7347ee91ddf9bbe96e94cc4eb
alexander.belzer <alexander.belzer@innogames.com>
2023-05-31 15:20:22 +0200

    no flag

--- Archery/Assets/Scripts/Voronoi/MeshCreator.cs
+++ Archery/Assets/Scripts/Voronoi/MeshCreator.cs
@@ -34,7 +34,6 @@ namespace Voronoi
 
             private const float Epsilon = 1e-5f;
 
-            [Flags]
             public enum PlaneType
             {
                 OnPlane, Front,Back,Spanning
